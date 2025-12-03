2c2913d2da9f112bcf1a841e020a4547b011b8d0
tccmw <jungmwoo6248@gmail.com>
2025-12-03 17:18:53 +0900

    feat(#8): signInComplete 기본세팅
    star가 메인임

---
+++ src/pages/auth/SignInComplete.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+function SignInComplete() {
+  return (
+    <View>
+      <View>
+        <Text>dasfd</Text>
+      </View>
+    </View>
+  );
+}
+
+export default SignInComplete;
