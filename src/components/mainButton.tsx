629a3669c153de806c8ca4e76cfba10c6c55d264
tccmw <jungmwoo6248@gmail.com>
2025-12-06 16:51:18 +0900

    feat(#16) : mainPage용 button 컴포넌트 추가

---
+++ src/components/mainButton.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Pressable, StyleSheet, Text } from 'react-native';
+
+interface ButtonProps {
+  onPress?: () => void;
+  disabled?: boolean;
+  label?: string;
+}
+
+const MainButton = ({ onPress, disabled, label }: ButtonProps) => {
+  return (
+    <Pressable
+      style={
+        label === '시작'
+          ? styles.StartButton
+          : disabled
+          ? styles.Button
+          : StyleSheet.compose(styles.Button, styles.ButtonActive)
+      }
+      disabled={disabled}
+      onPress={onPress}
+    >
+      <Text style={styles.ButtonText}>{label}</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  Button: {
+    backgroundColor: '#989898',
+    width: '100%',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 8,
+  },
+  ButtonActive: {
+    backgroundColor: '#007AFF',
+    shadowColor: '#007AFF',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 8,
+  },
+  StartButton: {
+    backgroundColor: '#272429',
+    width: '60%',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 50,
+    marginBottom: 15,
+  },
+  ButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default MainButton;
